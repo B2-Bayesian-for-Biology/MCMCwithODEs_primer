4b3ebfe5cb24793c2025d6dbea716d254f579989
David <dtalmy@utk.edu>
2025-05-01 11:07:03 -0400

    expanded figure 1

--- case_study_1/python/src/plotting.py
+++ case_study_1/python/src/plotting.py
@@ -35,13 +35,37 @@ if __name__ == "__main__":
 
     trace = az.from_netcdf('../data/posterior_trace.nc')
 
-    # Plot results
-    fig, ax = plt.subplots()
-    plot_posterior_predictive(ax, trace, time, obs)
-    ax.set_xlabel("Time")
-    ax.set_ylabel("Cell density")
-    ax.set_title("Posterior Predictive Fits")
-    fig.savefig('../figures/figure1')
-    plt.show()
+# Simulated PyMC trace data (replace with your actual inference result)
+idata = az.load_arviz_data("non_centered_eight")
 
+fig = plt.figure(figsize=(12, 15))
+outer_gs = fig.add_gridspec(3, 1, height_ratios=[4, 3,3])  # Top 1/2 and bottom 1/2
 
+# --- Top Half: Two Equal Columns ---
+gs_top = outer_gs[0].subgridspec(1, 2)
+ax_model = fig.add_subplot(gs_top[0, 0])
+ax_walks = fig.add_subplot(gs_top[0, 1])
+
+# --- Bottom Half: Two Rows, Each Split 2/3 and 1/3 ---
+gs_bottom = outer_gs[1:3].subgridspec(2, 3, width_ratios=[1, 1, 1])
+
+ax_trace1 = fig.add_subplot(gs_bottom[0, 0:2])
+ax_hist1 = fig.add_subplot(gs_bottom[0, 2])
+
+ax_trace2 = fig.add_subplot(gs_bottom[1, 0:2])
+ax_hist2 = fig.add_subplot(gs_bottom[1, 2])
+
+# Random walk scatter
+mum = trace.posterior["mum"].values.flatten()
+N0 = trace.posterior["N0"].values.flatten()
+ax_walks.scatter(mum, N0, s=1, alpha=0.5)
+
+az.plot_trace(trace, var_names=["mum"], axes=np.r_[[[ax_hist1],[ax_trace1]]].T, compact=True, show=False)
+az.plot_posterior(trace, var_names=["mum"], ax=ax_hist1, show=False)
+az.plot_trace(trace, var_names=["N0"], axes=np.r_[[[ax_hist2],[ax_trace2]]].T, compact=True, show=False)
+az.plot_posterior(trace, var_names=["N0"], ax=ax_hist2, show=False)
+plot_posterior_predictive(ax_model, trace, time, obs)
+
+plt.tight_layout()
+
+fig.savefig('../figures/figure1')
