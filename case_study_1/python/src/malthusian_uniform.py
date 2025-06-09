7be837b566a58da99d5d1bf80e208d4e8a1c3fdc
RaunakDey <deyraunak111@gmail.com>
2025-06-09 10:22:22 -0400

    recol

---
+++ case_study_1/python/src/malthusian_uniform.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.integrate import odeint
+import pymc as pm
+import arviz as az
+from pytensor.compile.ops import as_op
+import pytensor.tensor as pt
+
+# Define the differential equation system
+def cells_ode(y, t, params):
+    mum = params[0]
+    return [mum * y[0]]
+
+# Build and return a PyMC model
+def build_pymc_model(time, obs):
+
+    cell_model = pm.ode.DifferentialEquation(
+        func=cells_ode,
+        times=data['times'].values,
+        n_states=1,
+        n_theta=1,
+        t0=0
+    )
+
+    with pm.Model() as model:
+        # Priors
+        mum = pm.Normal('mum', mu=0.5, sigma=0.3)
+        N0 = pm.Lognormal('N0', mu=np.log(obs[0]), sigma=0.1)
+        sigma = pm.HalfNormal("sigma", 1)
+
+        y_hat = cell_model(y0=[N0], theta=[mum])
+        pm.Normal("Y_obs", mu=pm.math.log(y_hat[:, 0]), sigma=sigma, observed=np.log(obs))
+
+    return model
+
+
+# Run inference and return the trace (InferenceData)
+def run_inference(model, draws=1000, tune=1000, chains=4):
+    with model:
+        trace = pm.sample(draws=draws, tune=tune, chains=chains, return_inferencedata=True)
+    return trace
+
+# ---------------------------
+# MAIN EXECUTION
+# ---------------------------
+
+# This block only runs if the script is executed directly (not when imported)
+if __name__ == "__main__":
+    # Load data
+    data = pd.read_csv("./../data/phaeocystis_control.csv")
+    time = data['times'].values
+    obs = data['cells'].values
+
+    # Build and run model
+    model = build_pymc_model(time, obs)
+    trace = run_inference(model)
+
+    # save trace plots to csv
+    az.to_netcdf(trace, '../data/posterior_trace.nc')
+
---
+++ case_study_1/python/src/malthusian_uniform_plotting.py
@@ -0,0 +1,71 @@
+from malthusian_uniform import *
+import matplotlib.pyplot as plt
+import arviz as az
+
+# load trace
+growth_trace = az.from_netcdf('../data/posterior_trace.nc')
+
+############################################
+# trace plots
+############################################
+
+# plot chains - GROWTH
+axes = az.plot_trace(growth_trace)
+chain_colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red']
+for ax_row in axes:
+    for ax in ax_row:
+        lines = ax.get_lines()
+        for i, line in enumerate(lines):
+            line.set_color(chain_colors[i % len(chain_colors)])
+plt.tight_layout()
+plt.savefig('../figures/growth_chains')
+
+############################################
+# posteriors
+############################################
+
+# Plot posterior correlations
+az.plot_pair(growth_trace, kind='kde', divergences=True, marginals=True)
+plt.savefig('../figures/growth_posterior')
+
+############################################
+# autocorrelation
+############################################
+
+# 7 Convergence test for the chains - Gelman-Rubin, Geweke, and Autocorrelation
+rhat = az.rhat(growth_trace)
+autocorr = az.plot_autocorr(growth_trace)
+print(f'Rhat:\n{rhat}\n')
+plt.savefig('../figures/growth_autocorrelation')
+
+############################################
+# dynamics
+############################################
+
+# Plot the original data
+def plot_data(ax, time, obs, **kwargs):
+    ax.plot(time, obs, 'ko', label=r'$Phaeocystis$ $globosa$',zorder=1, **kwargs)
+
+# Plot a single model realization
+def plot_model(ax, model_output, time, **kwargs):
+    ax.plot(time, model_output, **kwargs)
+
+# Overlay posterior predictive simulations
+def plot_posterior_predictive(ax, trace, time, obs, num_samples=400, ode_solver=cells_ode, **kwargs):
+    df = az.extract(trace, num_samples=num_samples).to_dataframe()
+    plot_data(ax, time, obs, lw=0)
+    for _, row in df.iterrows():
+        model_output = odeint(ode_solver,[row["N0"]], time, args=([row["mum"]],))
+        plot_model(ax, model_output, time, lw=1, alpha=0.1, c='r',zorder=0, **kwargs)
+    ax.legend()
+    ax.semilogy()
+
+f,ax = plt.subplots()
+data = pd.read_csv("./../data/phaeocystis_control.csv")
+time = data['times'].values
+obs = data['cells'].values
+plot_posterior_predictive(ax, growth_trace, time, obs)
+
+f.savefig('../figures/growth_dynamics')
+
+
